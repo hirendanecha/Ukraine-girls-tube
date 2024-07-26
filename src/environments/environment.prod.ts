39e67de3c6129fba1a8091a90680877d454dd7ac
ppa-opash <ppa.opash@gmail.com>
2024-07-26 15:48:46 +0530

    initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,21 @@
+const url = 'https://api.ukrainiangirls.tube';
+const webUrl = 'https://ukrainiangirls.tube/';
+const tubeUrl = 'https://tube.ukrainiangirls.tube/'
+
+// const url = 'http://localhost:8080';
+// const webUrl = 'http://localhost:4200/';
+
+export const environment = {
+  production: false,
+  hmr: false,
+  serverUrl: `${url}/api/v1/`,
+  socketUrl: `${url}/`,
+  webUrl: webUrl,
+  tubeUrl: tubeUrl,
+  domain: '.ukrainiangirls.tube',
+  siteKey: '0x4AAAAAAATU1CBk_lvwGxIy',
+  secretKey: '0x4AAAAAAATU1GanFiWSflL_7a_cnZt_SKM',
+  qrLink: `${webUrl}settings/edit-profile/`,
+  EncryptIV: 8625401029409790,
+  EncryptKey: 8625401029409790,
+};
